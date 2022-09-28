30acdcd83273c6fe5a9796205c5a32563cb3b4e1
赤川大空 <daisuke1024akagawa@gmail.com>
2022-09-28 19:21:19 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
 setup(
     name="handycam",
     version="v1.0.0",
-    packages=["handycam"]
+    packages=["handycam"],
+    install_requires=_requires_from_file('requirements.txt'),
 )
